2b34c3fc2ccc48aab311d3402dcfa92943fa2fd2
Loreina Salalila <vialoraine.gg@gmail.com>
2020-11-30 12:58:15 -0800

    server.js index modification

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,23 +5,13 @@ var fs = require("fs");
 
 // Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Routes
-
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "public/index.html"));
-// });
-
-// app.get("/notes", function(req, res){
-//     res.sendFile(path.join(__dirname, "public/notes.html"));
-// });
+// ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
-}); 
+});
